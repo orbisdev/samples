96d4ad2d35c1fcc20a8dc10b1324e079403badf0
masterzorag <masterzorag@gmail.com>
2019-07-25 22:10:21 +0200

    new sample: MAPI_demo_r3
    - it switches between 2 SL programs on UP keypress
    - includes texture support (note: but included shaders does not uses texture)

---
+++ orbis_MAPI_samples/MAPI_demo_r3/source/MAPI_scene2.c
@@ -0,0 +1,402 @@
+/*****************************************************************************
+ * ==> Shader viewer --------------------------------------------------------*
+ *****************************************************************************
+ * Description : A shader viewer tool                                        *
+ * Developer   : Jean-Milost Reymond                                         *
+ * Copyright   : 2015 - 2018, this file is part of the Minimal API. You are  *
+ *               free to copy or redistribute this file, modify it, or use   *
+ *               it for your own projects, commercial or not. This file is   *
+ *               provided "as is", without ANY WARRANTY OF ANY KIND          *
+ *****************************************************************************/
+
+// std
+#include <stdio.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+// mini API
+#include "MiniCommon.h"
+#include "MiniGeometry.h"
+#include "MiniVertex.h"
+#include "MiniShapes.h"
+#include "MiniShader.h"
+#include "MiniRenderer.h"
+
+
+//pc_MAPI_samples # clang egl.c MAPI_shader_viewer.c -I$PS4SDK/include/MiniAPI -L.. -lMiniAPI -lm -lGL -lEGL -lX11 -D_MAPI_
+
+#include <orbisFile.h>
+
+#define ICHANNEL0_TEXTURE_FILE  "host0:iChannel0.png"
+
+//------------------------------------------------------------------------------
+// one _shared_ generic vertex shader program
+const char *g_pVertexShader =
+    "precision mediump float;"
+    "attribute vec3  mini_aPosition;"
+    "uniform   float mini_uTime;"
+    "uniform   vec2  mini_uResolution;"
+    "uniform   vec2  mini_uMouse;"
+    "varying   float iTime;"
+    "varying   vec2  iResolution;"
+    "varying   vec2  iMouse;"
+    ""
+    "void main(void)"
+    "{"
+    "    iResolution = mini_uResolution;"
+    "    iTime       = mini_uTime;"
+    "    iMouse      = mini_uMouse;"
+    "    gl_Position = vec4(mini_aPosition, 1.0);"
+    "}";
+
+// a first fragment shader (iTime sample)
+const char *g_pshadersource1 =
+    "precision mediump float; \
+     varying float iTime; \
+     varying vec2  iResolution; \
+     void main() \
+     { \
+         vec2 uv = gl_FragCoord.xy / iResolution.xy; \
+         vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4)); \
+         gl_FragColor = vec4(col,1.0); \
+     }";
+
+// a second fragment shader (flower_of_life_RGB)
+// (c) 2016 xoihazard
+const char *g_pshadersource =
+    "precision mediump float;\n"
+    "varying float iTime;\n"
+    "varying vec2  iResolution;\n"
+    "varying vec2  iMouse;\n"
+    "\n"
+    "#define TWO_PI 6.2831853072\n"
+    "#define PI 3.14159265359\n"
+    ""
+    "const float timeScale = 0.2;"
+    "const float displace = 0.01;"
+    "const float gridSize = 20.0;"
+    "const float wave = 5.0;"
+    "const float brightness = 1.5;"
+    ""
+    "vec2 rotate(in vec2 v, in float angle) {"
+    "    float c = cos(angle);"
+    "    float s = sin(angle);"
+    "    return v * mat2(c, -s, s, c);"
+    "}"
+    "vec3 coordToHex(in vec2 coord, in float scale, in float angle) {"
+    "   vec2 c = rotate(coord, angle);"
+    "   float q = (1.0 / 3.0 * sqrt(3.0) * c.x - 1.0 / 3.0 * c.y) * scale;"
+    "   float r = 2.0 / 3.0 * c.y * scale;"
+    "   return vec3(q, r, -q - r);"
+    "}"
+    "vec3 hexToCell(in vec3 hex, in float m) {"
+    "    return fract(hex / m) * 2.0 - 1.0;"
+    "}"
+    "float absMax(in vec3 v) {"
+    "    return max(max(abs(v.x), abs(v.y)), abs(v.z));"
+    "}"
+    "float nsin(in float value) {"
+    "    return sin(value * TWO_PI) * 0.5 + 0.5;"
+    "}"
+    "float hexToFloat(in vec3 hex, in float amt) {"
+    "    return mix(absMax(hex), 1.0 - length(hex) / sqrt(3.0), amt);"
+    "}"
+    "float calc(in vec2 tx, in float time) {"
+    "    float angle = PI * nsin(time * 0.1) + PI / 6.0;"
+    "    float len = 1.0 - length(tx) * nsin(time);"
+    "    float value = 0.0;"
+    "    vec3 hex = coordToHex(tx, gridSize * nsin(time * 0.1), angle);"
+    "    for (int i = 0; i < 3; i++) {"
+    "        float offset = float(i) / 3.0;"
+    "        vec3 cell = hexToCell(hex, 1.0 + float(i));"
+    "        value += nsin(hexToFloat(cell,nsin(len + time + offset)) *" 
+    "                  wave * nsin(time * 0.5 + offset) + len + time);"
+    "    }"
+    "    return value / 3.0;"
+    "}"
+    "void main(void)"
+    "{"
+    "    vec2 tx = (gl_FragCoord.xy / iResolution.xy) - 0.5;"
+    "    tx.x *= iResolution.x / iResolution.y;"
+    "    float time = iTime * timeScale;"
+    "    vec3 rgb = vec3(0.0, 0.0, 0.0);"
+    "    for (int i = 0; i < 3; i++) {"
+    "        float time2 = time + float(i) * displace;"
+    "        rgb[i] += pow(calc(tx, time2), 2.0);"
+    "    }"
+    "    gl_FragColor = vec4(rgb * brightness, 1.0);"
+    "}";
+
+//------------------------------------------------------------------------------
+#define NUM_OF_PROGRAMS  (2)
+
+MINI_Shader        g_Shader            [NUM_OF_PROGRAMS];
+GLuint             g_ShaderProgram     [NUM_OF_PROGRAMS];
+MINI_VertexFormat  g_VertexFormat      [NUM_OF_PROGRAMS];
+float*             g_pSurfaceVB        [NUM_OF_PROGRAMS];
+unsigned int       g_SurfaceVertexCount[NUM_OF_PROGRAMS];
+
+// shared between shaders
+const float        g_SurfaceWidth     = 10.0f;
+const float        g_SurfaceHeight    = 12.5f;
+float              g_Time             = 0.0f;
+GLuint             g_TimeSlot         = 0;
+GLuint             g_ResolutionSlot   = 0;
+MINI_Vector2       g_Resolution;      // dimension (x, y)
+GLuint             g_MouseSlot        = 0;
+MINI_Vector2       g_MousePos;        // position (x, y)
+
+/// second shader can use iChannel0.png as Texture
+GLuint             g_TextureIndex     = GL_INVALID_VALUE;
+GLuint             g_TexSamplerSlot   = 0; // iChannel0
+
+int                g_SceneInitialized = 0;
+//unsigned long long g_PreviousTime   = 0L;
+//------------------------------------------------------------------------------
+static void CreateViewport(float w, float h)
+{
+    // create the OpenGL viewport
+    glViewport(0, 0, w, h);
+
+    // set the screen resolution
+    g_Resolution.m_X = w;
+    g_Resolution.m_Y = h;
+}
+
+//------------------------------------------------------------------------------
+void InitScene_1(int w, int h)
+{
+    // we will use pad to hook mouse coord: reset center position
+    g_MousePos.m_X = g_MousePos.m_Y = 0.0f;
+
+    for(int i=0; i<NUM_OF_PROGRAMS; i++)
+    {
+           g_pSurfaceVB[i]         = 0;
+           g_SurfaceVertexCount[i] = 0;
+
+    // we can use OrbisGl shader wrappers, or MiniAPI ones
+    #define __PGL_ORBISGL__
+
+    #if defined __PGL_ORBISGL__
+
+        // compile, link and use shader
+        GLuint vertexShader;
+        GLuint fragmentShader;
+
+        // one shared vertex program source
+        vertexShader = orbisGlCompileShader(GL_VERTEX_SHADER, g_pVertexShader);
+        if (!vertexShader) {
+            debugNetPrintf(DEBUG, "Error during compiling vertex shader !\n");
+        }
+
+        // switch fragment program source
+        if(i == 0)
+        {
+            fragmentShader = orbisGlCompileShader(GL_FRAGMENT_SHADER, g_pshadersource);
+        } else {
+            fragmentShader = orbisGlCompileShader(GL_FRAGMENT_SHADER, g_pshadersource1);
+        }
+        if (!fragmentShader) {
+            debugNetPrintf(DEBUG, "Error during compiling fragment shader !\n");
+        }
+
+        g_ShaderProgram[i] = orbisGlLinkProgram(vertexShader, fragmentShader);
+        if (!g_ShaderProgram[i]) {
+            debugNetPrintf(DEBUG, "Error during linking shader ! program_id=%d (0x%08x)\n", g_ShaderProgram[i], g_ShaderProgram[i]);
+        }
+        debugNetPrintf(DEBUG, "g_ShaderProgram[%d]:%d\n", i, g_ShaderProgram[i]);
+
+    #else // use MiniAPI wrapper (link against MiniAPI lib)
+
+        // compile, link and use shader
+        g_ShaderProgram[i] = miniCompileShaders(g_pVertexShader, g_pshadersource);
+
+    #endif
+
+        if(!g_ShaderProgram[i]) { sleep(5); }
+
+        glUseProgram(g_ShaderProgram[i]);  // we setup this one
+
+        // get shader attributes
+        g_Shader[i].m_VertexSlot = glGetAttribLocation (g_ShaderProgram[i], "mini_aPosition");
+        g_TimeSlot               = glGetUniformLocation(g_ShaderProgram[i], "mini_uTime");
+        g_ResolutionSlot         = glGetUniformLocation(g_ShaderProgram[i], "mini_uResolution");
+        g_MouseSlot              = glGetUniformLocation(g_ShaderProgram[i], "mini_uMouse");
+        // for texture
+        g_TexSamplerSlot         = glGetAttribLocation( g_ShaderProgram[i], "iChannel0");
+
+        // create the viewport
+        CreateViewport(w, h);
+        debugNetPrintf(DEBUG, "g_ResolutionSlot: %f %f\n", g_Resolution.m_X, g_Resolution.m_Y);
+
+        // notify shader about screen size
+        glUniform2f(g_ResolutionSlot, g_Resolution.m_X, g_Resolution.m_Y);
+
+        // initialize the mouse (or finger) position in the shader
+        glUniform2f(g_MouseSlot, 0, 0);
+
+        // configure OpenGL depth testing
+        glEnable(GL_DEPTH_TEST);
+        glDepthMask(GL_TRUE);
+        glDepthFunc(GL_LEQUAL);
+        glDepthRangef(0.0f, 1.0f);
+
+        // enable culling
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_FRONT);
+        glFrontFace(GL_CCW);
+
+        g_VertexFormat[i].m_UseNormals  = 0;
+        g_VertexFormat[i].m_UseTextures = 0;
+        g_VertexFormat[i].m_UseColors   = 0;
+
+        // generate surface
+        miniCreateSurface(&g_SurfaceWidth,
+                          &g_SurfaceHeight,
+                           0xFFFFFFFF,
+                          &g_VertexFormat[i],
+                          &g_pSurfaceVB[i],
+                          &g_SurfaceVertexCount[i]);
+        if(i == 1)
+        {
+            // load iChannel0 texture
+            g_TextureIndex = load_png_asset_into_texture(ICHANNEL0_TEXTURE_FILE);
+            debugNetPrintf(DEBUG, "load_png_asset_into_texture ret: %d\n", g_TextureIndex);
+
+            glActiveTexture(GL_TEXTURE0);
+            glUniform1i(g_TexSamplerSlot, GL_TEXTURE0);
+        }
+    }
+    g_SceneInitialized = 1;
+}
+//------------------------------------------------------------------------------
+void DeleteScene_1()
+{
+    g_SceneInitialized = 0;
+    for(int i=0; i<NUM_OF_PROGRAMS; i++)
+    {
+        // delete surface vertices
+        if (g_pSurfaceVB[i])
+        {
+            free(g_pSurfaceVB[i]);
+            g_pSurfaceVB[i] = 0;
+        }
+
+        // iChannel0
+        if (g_TextureIndex != GL_INVALID_VALUE)
+            glDeleteTextures(1, &g_TextureIndex);
+
+        g_TextureIndex = GL_INVALID_VALUE;
+
+        // delete shader program
+        if (g_ShaderProgram[i])
+            glDeleteProgram(g_ShaderProgram[i]);
+
+        g_ShaderProgram[i] = 0;
+    }
+}
+//------------------------------------------------------------------------------
+static float frame = 0.;
+extern float p1_pos_x, p1_pos_y; // from main()
+void UpdateScene_1(float elapsedTime)
+{
+    elapsedTime = frame + 0.05f;
+
+    g_Time += elapsedTime * 0.5f;  // calculate next time
+
+    for(int i=0; i<NUM_OF_PROGRAMS; i++)
+    {
+        glUseProgram(g_ShaderProgram[i]);
+
+        // notify shader about elapsed time
+        glUniform1f(g_TimeSlot, g_Time);
+
+        // report
+        #ifdef __PS4__
+        // notify shader about mouse position
+        glUniform2f(g_MouseSlot, g_MousePos.m_X, g_MousePos.m_Y);
+        //debugNetPrintf(DEBUG, "g_Time = %f %f\n", g_Time, sin(g_Time));
+
+        #else
+        // notify shader about mouse position
+        glUniform2f(g_MouseSlot, p1_pos_x, p1_pos_y);
+        //printf("g_Time = %f %f\n", g_Time, sin(g_Time));
+        //printf("update_pos: %f %f\n", p1_pos_x, p1_pos_y);
+
+        #endif
+    }
+}
+//------------------------------------------------------------------------------
+int scene_num = 0;
+void DrawScene_1(void)
+{
+    int i = scene_num %NUM_OF_PROGRAMS;
+    {
+        glUseProgram(g_ShaderProgram[i]);
+
+        if(i == 1)
+        {
+            // refresh state for this shader
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, g_TextureIndex);
+        }
+
+        // configure OpenGL depth testing
+        glEnable(GL_DEPTH_TEST);
+        glDepthMask(GL_TRUE);
+        glDepthFunc(GL_LEQUAL);
+        glDepthRangef(0.0f, 1.0f);
+
+        // enable culling
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_FRONT);
+        glFrontFace(GL_CCW);
+
+        //miniBeginScene(0.0f, 0.0f, 0.0f, 1.0f);// we already clean in main render loop
+
+        // draw the surface on which the shader will be executed
+        miniDrawSurface(g_pSurfaceVB[i],
+                        g_SurfaceVertexCount[i],
+                       &g_VertexFormat[i],
+                       &g_Shader[i]);
+    }
+    //miniEndScene(); // a stub, does nothing
+}
+//------------------------------------------------------------------------------
+#ifdef __PS4__
+// hook pad over miniMouse vec2(x, y)
+// we get feedback on each keypress
+#define STEP  (0.005)  // a small positive delta
+void pad_special(int special)
+{
+    switch (special) {
+        case 0: //_KEY_LEFT:
+            if(g_MousePos.m_X > -1.000) g_MousePos.m_X -= STEP;
+            break;
+        case 1: //_KEY_RIGHT:
+            if(g_MousePos.m_X < 1.000)  g_MousePos.m_X += STEP;
+            break;
+        case 2: //_KEY_UP:
+            if(g_MousePos.m_Y < 1.000)  g_MousePos.m_Y += STEP;
+            scene_num++;
+            sceKernelUsleep(900044);
+            //debugNetPrintf(DEBUG, "scene_num = %d %d\n", scene_num, scene_num %2);
+            break;
+        case 3: //_KEY_DOWN:
+            if(g_MousePos.m_Y > -1.000) g_MousePos.m_Y -= STEP;
+            break;
+        /*
+        case 4: //_BUTTON_X:
+            DeleteScene_1();
+            InitScene_1(ATTR_ORBISGL_WIDTH,ATTR_ORBISGL_HEIGHT);
+            debugNetPrintf(DEBUG, "Re-Init Scene!\n");
+            break;
+        */
+        default:
+            /*do nothing*/
+            break;
+   }
+   debugNetPrintf(DEBUG, "update_pos: %f %f\n", g_MousePos.m_X, g_MousePos.m_Y);
+}
+#endif
---
+++ orbis_MAPI_samples/MAPI_demo_r3/source/demo-font.c
@@ -0,0 +1,336 @@
+/*
+code build and run on linux or ps4, following __PS4__ definition:
+liborbisGl, libfreetype and libfreetype-gl are required
+on pc runs with GLUT
+
+clang \
+mat4.c \
+vector.c \
+texture-font.c \
+texture-atlas.c \
+vertex-attribute.c \
+vertex-buffer.c \
+demo-font.c \
+-lGL -lm -lX11 -lEGL  -I$PS4SDK/include/freetype2 -lfreetype -lglut -v -o demo-font
+
+ clang source/ *.c -lGL -lm -lX11 -lEGL  -I$PS4SDK/include/freetype2 -lfreetype -lglut -o demo-font
+*/
+
+/* ============================================================================
+ * Freetype GL - A C OpenGL Freetype engine
+ * Platform:    Any
+ * WWW:         http://code.google.com/p/freetype-gl/
+ * ----------------------------------------------------------------------------
+ * Copyright 2011,2012 Nicolas P. Rougier. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NICOLAS P. ROUGIER ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL NICOLAS P. ROUGIER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Nicolas P. Rougier.
+ * ============================================================================
+ *
+ * Example showing regular font usage
+ *
+ * ============================================================================
+ */
+#include <stdio.h>
+#include <wchar.h>
+
+#include <freetype-gl.h>  // links against libfreetype-gl
+
+#if defined(__APPLE__)
+    #include <Glut/glut.h>
+#elif defined(_WIN32) || defined(_WIN64)
+    #include <GLUT/glut.h>
+#elif defined(__PS4__)
+    #include <debugnet.h>
+    #include <orbisFile.h>
+#else
+    #include <GL/glut.h>
+#endif
+
+
+//missing guards
+//#include "MiniShader.h"  // miniCompileShaders
+
+// ------------------------------------------------------- typedef & struct ---
+typedef struct {
+    float x, y, z;    // position
+    float s, t;       // texture
+    float r, g, b, a; // color
+} vertex_t;
+
+
+// ------------------------------------------------------- global variables ---
+GLuint shader;
+texture_atlas_t *atlas;
+vertex_buffer_t *buffer;
+mat4   model, view, projection;
+
+// ---------------------------------------------------------------- reshape ---
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    mat4_set_orthographic( &projection, 0, width, 0, height, -1, 1);
+}
+
+// ---------------------------------------------------------------- display ---
+void render_text( void )
+{
+    // we already clean in main renderloop()!
+
+    glUseProgram( shader );
+    glBindTexture( GL_TEXTURE_2D, atlas->id );  // rebind
+
+    glDisable(GL_CULL_FACE);  // just in case, disable it
+
+    // freetype-gl shaders wanted config
+    glEnable( GL_BLEND );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+
+    //glUseProgram( shader );
+    {
+        glUniform1i(        glGetUniformLocation( shader, "texture" ),    0 );
+        glUniformMatrix4fv( glGetUniformLocation( shader, "model" ),      1, 0, model.data);
+        glUniformMatrix4fv( glGetUniformLocation( shader, "view" ),       1, 0, view.data);
+        glUniformMatrix4fv( glGetUniformLocation( shader, "projection" ), 1, 0, projection.data);
+        vertex_buffer_render( buffer, GL_TRIANGLES );
+    }
+
+    glDisable( GL_BLEND );  // Reset state back
+
+    // we already swapframe in main renderloop()!
+}
+
+
+// --------------------------------------------------------------- add_text ---
+void add_text( vertex_buffer_t * buffer, texture_font_t * font,
+               wchar_t * text, vec4 * color, vec2 * pen )
+{
+    size_t i;
+    float r = color->red, g = color->green, b = color->blue, a = color->alpha;
+    for( i=0; i<wcslen(text); ++i )
+    {
+        texture_glyph_t *glyph = texture_font_get_glyph( font, text[i] );
+        if( glyph != NULL )
+        {
+            int kerning = 0;
+            if( i > 0)
+            {
+                kerning = texture_glyph_get_kerning( glyph, text[i-1] );
+            }
+            pen->x += kerning;
+            int x0  = (int)( pen->x + glyph->offset_x );
+            int y0  = (int)( pen->y + glyph->offset_y );
+            int x1  = (int)( x0 + glyph->width );
+            int y1  = (int)( y0 - glyph->height );
+            float s0 = glyph->s0;
+            float t0 = glyph->t0;
+            float s1 = glyph->s1;
+            float t1 = glyph->t1;
+            GLuint indices[6] = {0,1,2, 0,2,3};
+            vertex_t vertices[4] = { { x0,y0,0,  s0,t0,  r,g,b,a },
+                                     { x0,y1,0,  s0,t1,  r,g,b,a },
+                                     { x1,y1,0,  s1,t1,  r,g,b,a },
+                                     { x1,y0,0,  s1,t0,  r,g,b,a } };
+            vertex_buffer_push_back( buffer, vertices, 4, indices, 6 );
+            pen->x += glyph->advance_x;
+        }
+    }
+}
+// ------------------------------------------------------ freetype-gl shaders ---
+GLuint CreateProgram(void)
+{
+    GLuint programID = 0;
+
+    const GLchar s_vertex_shader_code[] =
+       "precision mediump float; \
+        uniform mat4 model; \
+        uniform mat4 view; \
+        uniform mat4 projection; \
+        \
+        attribute vec3 vertex; \
+        attribute vec2 tex_coord; \
+        attribute vec4 color; \
+        \
+        varying vec2 vTexCoord; \
+        varying vec4 fragColor; \
+        \
+        void main() \
+        { \
+            vTexCoord.xy = tex_coord.xy; \
+            fragColor    = color; \
+            gl_Position  = projection*(view*(model*vec4(vertex,1.0))); \
+        }";
+
+    const GLchar s_fragment_shader_code[] =
+       "precision mediump float; \
+        uniform sampler2D texture; \
+        \
+        varying vec2 vTexCoord; \
+        varying vec4 fragColor; \
+        \
+        void main() \
+        { \
+            float a = texture2D(texture, vTexCoord).a; \
+            gl_FragColor = vec4(fragColor.rgb, fragColor.a*a); \
+        }";
+
+    // we can use OrbisGl wrappers, or MiniAPI ones
+//#define __PGL_ORBISGL__
+
+#if defined __PGL_ORBISGL__
+    GLuint vertexShader;
+    GLuint fragmentShader;
+
+    vertexShader = orbisGlCompileShader(GL_VERTEX_SHADER, s_vertex_shader_code);
+    if (!vertexShader) {
+        debugNetPrintf(DEBUG, "Error during compiling vertex shader !\n");
+    }
+
+    fragmentShader = orbisGlCompileShader(GL_FRAGMENT_SHADER, s_fragment_shader_code);
+    if (!fragmentShader) {
+        debugNetPrintf(DEBUG, "Error during compiling fragment shader !\n");
+    }
+
+    programID = orbisGlLinkProgram(vertexShader, fragmentShader);
+    if (!programID) {
+        debugNetPrintf(DEBUG, "Error during linking shader ! program_id=%d (0x%08x)\n", programID, programID);
+    }
+
+#else // links MiniAPI !
+    programID = miniCompileShaders(s_vertex_shader_code, s_fragment_shader_code);
+
+#endif
+    // feedback
+    //debugNetPrintf(DEBUG, "program_id=%d (0x%08x)\n", programID, programID);
+    return programID;
+}
+
+// libfreetype-gl pass last composed Text_Length in pixel, we use to align text
+extern float tl;
+// ------------------------------------------------------------------- main ---
+int es2sample_init( int argc, char **argv )
+{
+    //debugNetPrintf(DEBUG,"es2sample_init\n");
+
+    size_t h; // text size in pt
+    texture_font_t  *font  = 0;
+    atlas = texture_atlas_new( 1024, 1024, 1 );  // a bigger atlas, stores more glyphs
+    //debugNetPrintf(DEBUG,"atlas at %p\n", atlas);
+
+    const char * filename  = "host0:SoMAItalicBold.ttf";
+
+    wchar_t *text = L"OrbisLinkGL demo: GL ES 2.0 shaders, FreeType";
+
+    buffer = vertex_buffer_new( "vertex:3f,tex_coord:2f,color:4f" );
+    //debugNetPrintf(DEBUG,"buffer at %p\n", buffer);
+
+    int tx = 20; // text position
+
+    vec2 pen = {{500, 600}}; // init pen: 0,0 is lower left
+
+    vec4 black = {{ 0.1, 0.1, 0.4, 1 }}; // RGBA color
+    vec4 white = {{ 1,   1,   1,   1 }}; // RGBA color
+    vec4 col   = {{ 1,   0.2, 1,   1 }}; // RGBA color
+
+#ifdef __PS4__
+    font = texture_font_new( atlas, filename, 32 );  // create a font
+
+    // 1.
+    wchar_t *s = L"this sample show aligned text";   // set text
+
+    texture_font_load_glyphs( font, s );        // set textures
+    pen.x = (ATTR_ORBISGL_WIDTH - tl) /2;       // use Text_Length to align pen.x
+    add_text( buffer, font, s, &white, &pen );  // set vertexes
+
+    // 2.
+    s = L"liborbis freetype demo";              // set text
+    texture_font_load_glyphs( font, s );        // set textures
+    pen.x = (ATTR_ORBISGL_WIDTH - tl);          // use Text_Length to align pen.x (right)
+    pen.y -= font->height;                      // 1 line down!
+    add_text( buffer, font, s, &white, &pen );  // set vertexes
+
+    texture_font_delete( font );    // end with font, cleanup
+
+    if(1)
+    {
+        font = texture_font_new( atlas, "host0:razors.ttf", 36 );  // create a font
+        wchar_t *s = L"make#liborbis";            // set text
+        texture_font_load_glyphs( font, s );      // set textures
+        pen.x = (ATTR_ORBISGL_WIDTH - tl) /2;     // set x position
+        pen.y = 900;                              // set y position
+        add_text( buffer, font, s, &col, &pen );  // set vertexes
+        texture_font_delete( font );              // cleanup font
+    }
+#endif
+    pen.y = 800;
+    for( h=12; h < 72; h += 6)
+    {
+        font = texture_font_new( atlas, "host0:SoMALightItalic.ttf", h );
+        //debugNetPrintf(DEBUG,"font at %p\n", font);
+        // reset pen, 1 line down
+        pen.x = tx;
+        pen.y -= font->height;
+
+        texture_font_load_glyphs( font, text );
+
+        // use Text_Length to align pen.x
+        pen.x = (ATTR_ORBISGL_WIDTH - tl) /2;
+        add_text( buffer, font, text, &white, &pen );
+
+        texture_font_delete( font );
+    }
+    glBindTexture( GL_TEXTURE_2D, atlas->id );
+
+    // compile, link and use shader
+    shader = CreateProgram();
+
+    if(!shader)
+    {
+        debugNetPrintf(DEBUG,"program creation failed\n");
+    }
+
+    mat4_set_identity( &projection );
+    mat4_set_identity( &model );
+    mat4_set_identity( &view );
+
+
+    reshape(ATTR_ORBISGL_WIDTH, ATTR_ORBISGL_HEIGHT);
+
+    return 0;
+}
+
+void es2sample_end(void)
+{
+    // destruct buffer, atlas
+    glDeleteTextures( 1, &atlas->id );
+    atlas->id = 0;
+    texture_atlas_delete( atlas );
+
+    if (shader)
+        glDeleteProgram(shader);
+
+    shader = 0;
+    return;
+}
